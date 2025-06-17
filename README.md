a69e8b4e9058017758ecdc5fd1ce323e2f65eb43
eelias13 <maierelias13@gmai.com>
2025-06-17 14:20:32 +0200

    update

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # My Website
 
-this is the content repo for my personal [website](https://eelias13.github.io/) 
+this is the content repo for my personal [website](https://elias-2001-de.github.io/) 
 
-I use the template I build [astro-spotlight](https://github.com/eelias13/astro-spotlight)
+I use the template I build [astro-spotlight](https://github.com/elias-2001-de/astro-spotlight)
 
 ## build and publish
 
--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-curl -L -o jonah -H "User-Agent: Mozilla/5.0" https://github.com/eelias13/jonah/releases/download/v0.1.5/jonah
+curl -L -o jonah -H "User-Agent: Mozilla/5.0" https://github.com/elias-2001-de/jonah/releases/download/v0.1.5/jonah
 chmod +x jonah
 
 mkdir -p build
 cd build
 
-git clone https://github.com/eelias13/astro-spotlight template  --depth 1
+git clone https://github.com/elias-2001-de/astro-spotlight template  --depth 1
 rm -r template/public
 cp -r ../public ./template
 
@@ -13,7 +13,7 @@ cd ..
 ./jonah project jonah_build.toml build/template
 
 
-git clone https://github.com/eelias13/eelias13.github.io website --depth 1
+git clone https://github.com/elias-2001-de/elias-2001-de.github.io website --depth 1
 cd website
 shopt -s extglob # enables extended pattern matching 
 rm -rf !(.git)
@@ -25,7 +25,8 @@ cd ..
 cp -r website/en/projects/open-gal     website/de/projects/open-gal
 cp -r website/en/projects/func-checker website/de/projects/func-checker
 cp -r website/en/projects/oberon0c     website/de/projects/oberon0c
+touch website/.nojekyll
 
-cd website
-git add . 
-git commit -m "[CI JOB]: update website"
+# cd website
+# git add .
+# git commit -m "[CI JOB]: update website"
